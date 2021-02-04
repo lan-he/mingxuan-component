7977326769bcb6c8f3ee3cdd9b7404c53c02aade
Lan <15669147150@163.com>
2021-02-04 17:44:59 +0800

    修改内容

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
+    <!-- <p class="arktitle">欣欣小年快乐</p> -->
     <div class="background-phone">
       <div class="phone-conent">
-        <StatusBar/>
+        <StatusBar />
         <router-view></router-view>
       </div>
       <div class="home-key" @click="back"></div>
     </div>
-    
   </div>
 </template>
 
@@ -15,17 +15,16 @@
 import StatusBar from '@/components/status-bar'
 export default {
   data() {
-    return {
-    }
+    return {}
   },
   components: {
-    StatusBar
+    StatusBar,
   },
   methods: {
     back() {
       this.$router.go(-1)
-    }
-  }
+    },
+  },
 }
 </script>
 
@@ -34,7 +33,8 @@ export default {
   width: 350px;
   height: 760px;
   margin: 0 auto;
-  background: url('https://img-1256953833.cos.ap-nanjing.myqcloud.com/phone-bg.png') no-repeat;
+  background: url('https://img-1256953833.cos.ap-nanjing.myqcloud.com/phone-bg.png')
+    no-repeat;
   background-size: 100% 100%;
   display: flex;
   align-items: center;
@@ -46,7 +46,8 @@ export default {
   width: 326px;
   height: 570px;
   position: relative;
-  background: url('https://img-1256953833.cos.ap-nanjing.myqcloud.com/wallpaper.jpg') no-repeat;
+  background: url('https://img-1256953833.cos.ap-nanjing.myqcloud.com/wallpaper.jpg')
+    no-repeat;
   background-size: 100% 100%;
 }
 .home-key {
@@ -56,5 +57,74 @@ export default {
   border-radius: 50%;
   cursor: pointer;
   bottom: 25px;
+}
+
+.arktitle {
+  text-align: center;
+  font-size: 36px;
+  font-weight: bold;
+  background: linear-gradient(to right, red, blue);
+  -webkit-background-clip: text;
+  color: transparent;
+  animation: blink 2s linear infinite;
+  -webkit-animation: blink 2s linear infinite;
+  -moz-animation: blink 2s linear infinite;
+  -ms-animation: blink 2s linear infinite;
+  -o-animation: blink 2s linear infinite;
+}
+@keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 0.8;
+  }
+}
+@-webkit-keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 0.8;
+  }
+}
+@-moz-keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 0.8;
+  }
+}
+@-ms-keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 0.8;
+  }
+}
+@-o-keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 0.8;
+  }
 }
 </style>
